78c0350418cec0b5a09a41c71ac1f1d8395691e1
kenmerav <ken@meravinteriors.com>
2026-03-06 19:07:57 -0700

    Fix blank screen by importing RequireAuth in app routes

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { AuthProvider } from "./contexts/AuthContext";
+import { RequireAuth } from "./components/auth/RequireAuth";
 import { RequireAdmin } from "./components/auth/RequireAdmin";
 import { RequireProfileComplete } from "./components/auth/RequireProfileComplete";
 import { RequireSubscription } from "./components/auth/RequireSubscription";
